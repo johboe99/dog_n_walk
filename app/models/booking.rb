d82723436dbeb5956c1ba4f4b0862e2c37a0a539
Ajmal Khan <ajmalkhan0413@gmail.com>
2024-11-19 15:36:03 +0100

    The booking model is completed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :dog
-  has_many :user
-  has_many :dog
 
   validates :date, presence: true
 end
--- db/migrate/20241119132054_add_comment_to_booking.rb
+++
@@ -1,5 +0,0 @@
-class AddCommentToBooking < ActiveRecord::Migration[7.1]
-  def change
-    add_column :bookings, :date, :date
-  end
-end
