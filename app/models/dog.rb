317ae475523480c6507e117fa9539b1c4def3220
Johannes Boehmer <j.boehmer@live.de>
2024-11-19 14:26:44 +0100

    added name validation for dog

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,4 +3,5 @@ class Dog < ApplicationRecord
   validates :breed, presence: true
   validates :age, presence: true
   validates :description, presence: true, length: { minimum: 5 }
+  validates :name, presence: true
 end
