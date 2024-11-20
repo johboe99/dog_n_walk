f789f0803a0768284486001adab19c35f0ce31ae
Johannes Boehmer <j.boehmer@live.de>
2024-11-20 12:42:23 +0100

    bookings new/create and a placeholder index method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,17 +6,25 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
     @user = current_user.id
-    @dog = Dog.find(params[:id])
+    @dog = Dog.find(params[:dog_id])
   end
 
   def create
     @booking = Booking.new(booking_params)
-    @user = current_user.id
-    @dog = Dog.find(params[:id])
+    @user = current_user
+    @booking.user = @user
+    @dog = Dog.find(params[:dog_id])
+    @booking.dog = @dog
     if @booking.save
-      redirect_to bookings_path, notice: "You will be going on a walk soon."
+      redirect_to dog_path(@dog), notice: "You will be going on a walk soon."
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date)
   end
 end
