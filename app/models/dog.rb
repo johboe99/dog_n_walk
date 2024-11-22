3279deaf95f42f8170b1073479b97698a46c46d4
Johannes Boehmer <j.boehmer@live.de>
2024-11-22 14:59:57 +0100

    dependent destory for dogs and bookings

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,6 @@
 class Dog < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
   validates :breed, presence: true
   validates :age, presence: true
